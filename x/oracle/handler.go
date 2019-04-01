2337f61c554e95916071284b135eba8212aeea23
Do Kwon <do@terra.money>
2019-04-01 22:46:23 +0900

    Update handler.go

--- x/oracle/handler.go
+++ x/oracle/handler.go
@@ -1,7 +1,6 @@
 package oracle
 
 import (
-	"fmt"
 	"terra/types"
 	"terra/x/oracle/tags"
 
@@ -61,7 +60,6 @@ func dropBallot(ctx sdk.Context, k Keeper, denom string, params Params) sdk.Tags
 
 // ballot for the asset is passing the threshold amount of voting power
 func ballotIsPassing(totalPower sdk.Int, ballot PriceBallot, params Params) bool {
-	fmt.Println(params.VoteThreshold.String(), totalPower.String(), ballot.TotalPower())
 	thresholdVotes := params.VoteThreshold.MulInt(totalPower).RoundInt()
 	return ballot.TotalPower().GTE(thresholdVotes)
 }
