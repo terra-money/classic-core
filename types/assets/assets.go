9e0d71f8a6da8cf5e944f089763087c1114a1ec9
Yun <yun@terra.money>
2019-04-02 17:51:40 +0900

    wip

--- types/assets/assets.go
+++ types/assets/assets.go
@@ -13,3 +13,15 @@ const (
 
 	MicroUnit = int64(1e6)
 )
+
+// IsValidAsset returns the asset is valid or not
+func IsValidAsset(denom string) bool {
+	return denom == MicroLunaDenom ||
+		denom == MicroUSDDenom ||
+		denom == MicroKRWDenom ||
+		denom == MicroSDRDenom ||
+		denom == MicroCNYDenom ||
+		denom == MicroJPYDenom ||
+		denom == MicroEURDenom ||
+		denom == MicroGBPDenom
+}
