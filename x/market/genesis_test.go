564fce547208e19798abfa05aaaa97d173c8bb27
Yun <yun@terra.money>
2019-09-27 18:04:49 +0900

    add genesis test to market & treasury module

---
+++ x/market/genesis_test.go
@@ -0,0 +1,24 @@
+package market
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/terra-project/core/x/market/internal/keeper"
+)
+
+func TestExportInitGenesis(t *testing.T) {
+	input := keeper.CreateTestInput(t)
+	input.MarketKeeper.SetBasePool(input.Ctx, sdk.NewDec(1123))
+	input.MarketKeeper.SetTerraPool(input.Ctx, sdk.NewDec(1123))
+	input.MarketKeeper.SetLastUpdateHeight(input.Ctx, int64(53242))
+	genesis := ExportGenesis(input.Ctx, input.MarketKeeper)
+
+	newInput := keeper.CreateTestInput(t)
+	InitGenesis(newInput.Ctx, newInput.MarketKeeper, genesis)
+	newGenesis := ExportGenesis(newInput.Ctx, newInput.MarketKeeper)
+
+	require.Equal(t, genesis, newGenesis)
+}
