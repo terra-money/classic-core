2da81aaebf9a4c7dfe8e758327223df6d0ba6e73
Yun <yun@terra.money>
2019-09-27 14:12:26 +0900

    update to cosmos-v0.37.1

--- sims.mk
+++ sims.mk
@@ -3,60 +3,65 @@
 ########################################
 ### Simulations
 
+BINDIR ?= $(GOPATH)/bin
 SIMAPP = github.com/terra-project/core/app
 
-sim-terra-nondeterminism:
-	@echo "Running nondeterminism test..."
+test-sim-nondeterminism:
+	@echo "Running non-determinism test..."
 	@go test -mod=readonly $(SIMAPP) -run TestAppStateDeterminism -Enabled=true \
-		-NumBlocks=100 -BlockSize=200 -Commit=true -v -timeout 24h
+		-NumBlocks=100 -BlockSize=200 -Commit=true -Period=0 -v -timeout 24h
 
-sim-terra-custom-genesis-fast:
+test-sim-custom-genesis-fast:
 	@echo "Running custom genesis simulation..."
 	@echo "By default, ${HOME}/.terrad/config/genesis.json will be used."
-	@go test -mod=readonly $(SIMAPP) -run TestFullAppSimulation -Genesis=${HOME}/.gaiad/config/genesis.json \
+	@go test -mod=readonly $(SIMAPP) -run TestFullAppSimulation -Genesis=${HOME}/.terrad/config/genesis.json \
 		-Enabled=true -NumBlocks=100 -BlockSize=200 -Commit=true -Seed=99 -Period=5 -v -timeout 24h
-
-sim-terra-fast:
-	@echo "Running quick Terra simulation. This may take several minutes..."
+test-sim-fast:
+	@echo "Running quick simulation. This may take several minutes..."
 	@go test -mod=readonly $(SIMAPP) -run TestFullAppSimulation -Enabled=true -NumBlocks=100 -BlockSize=200 -Commit=true -Seed=99 -Period=5 -v -timeout 24h
 
-sim-terra-import-export: runsim
-	@echo "Running Terra import/export simulation. This may take several minutes..."
-	$(GOPATH)/bin/runsim -SimAppPkg $(SIMAPP) 25 5 TestAppImportExport
 
-sim-terra-simulation-after-import: runsim
-	@echo "Running Terra simulation-after-import. This may take several minutes..."
-	$(GOPATH)/bin/runsim -SimAppPkg $(SIMAPP) 25 5 TestAppSimulationAfterImport
+test-sim-import-export: runsim
+	@echo "Running App import/export simulation. This may take several minutes..."
+	@$(BINDIR)/runsim -Jobs=4 -SimAppPkg=$(SIMAPP) 25 5 TestAppImportExport
 
-sim-terra-custom-genesis-multi-seed: runsim
+test-sim-after-import: runsim
+	@echo "Running App simulation-after-import. This may take several minutes..."
+	@$(BINDIR)/runsim -Jobs=4 -SimAppPkg=$(SIMAPP) 25 5 TestAppSimulationAfterImport
+
+test-sim-custom-genesis-multi-seed: runsim
 	@echo "Running multi-seed custom genesis simulation..."
 	@echo "By default, ${HOME}/.terrad/config/genesis.json will be used."
-	$(GOPATH)/bin/runsim -g ${HOME}/.terrad/config/genesis.json $(SIMAPP) 400 5 TestFullAppSimulation
+	@$(BINDIR)/runsim -Jobs=4 -Genesis=${HOME}/.terrad/config/genesis.json 400 5 TestFullAppSimulation
 
-sim-terra-multi-seed: runsim
-	@echo "Running multi-seed Terra simulation. This may take awhile!"
-	$(GOPATH)/bin/runsim -SimAppPkg $(SIMAPP) 400 5 TestFullAppSimulation
+test-sim-multi-seed-long: runsim
+	@echo "Running multi-seed application simulation. This may take awhile!"
+	@$(BINDIR)/runsim -Jobs=4 -SimAppPkg=$(SIMAPP) 500 50 TestFullAppSimulation
 
-sim-benchmark-invariants:
+test-sim-multi-seed-short: runsim
+	@echo "Running multi-seed application simulation. This may take awhile!"
+	@$(BINDIR)/runsim -Jobs=4 -SimAppPkg=$(SIMAPP) 50 10 TestFullAppSimulation
+
+test-sim-benchmark-invariants:
 	@echo "Running simulation invariant benchmarks..."
-	@go test -mod=readonly github.com/terra-project/core/app -benchmem -bench=BenchmarkInvariants -run=^$ \
+	@go test -mod=readonly $(SIMAPP) -benchmem -bench=BenchmarkInvariants -run=^$ \
 	-Enabled=true -NumBlocks=1000 -BlockSize=200 \
 	-Commit=true -Seed=57 -v -timeout 24h
 
 SIM_NUM_BLOCKS ?= 500
 SIM_BLOCK_SIZE ?= 200
 SIM_COMMIT ?= true
-sim-terra-benchmark:
-	@echo "Running Terra benchmark for numBlocks=$(SIM_NUM_BLOCKS), blockSize=$(SIM_BLOCK_SIZE). This may take awhile!"
-	@go test -mod=readonly -benchmem -run=^$$ github.com/terra-project/core/app -bench ^BenchmarkFullAppSimulation$$  \
+
+test-sim-benchmark:
+	@echo "Running App benchmark for numBlocks=$(SIM_NUM_BLOCKS), blockSize=$(SIM_BLOCK_SIZE). This may take awhile!"
+	@go test -mod=readonly -benchmem -run=^$$ $(SIMAPP) -bench ^BenchmarkFullAppSimulation$$  \
 		-Enabled=true -NumBlocks=$(SIM_NUM_BLOCKS) -BlockSize=$(SIM_BLOCK_SIZE) -Commit=$(SIM_COMMIT) -timeout 24h
 
-sim-terra-profile:
-	@echo "Running Terra benchmark for numBlocks=$(SIM_NUM_BLOCKS), blockSize=$(SIM_BLOCK_SIZE). This may take awhile!"
-	@go test -mod=readonly -benchmem -run=^$$ github.com/terra-project/core/app -bench ^BenchmarkFullAppSimulation$$ \
+test-sim-profile:
+	@echo "Running App benchmark for numBlocks=$(SIM_NUM_BLOCKS), blockSize=$(SIM_BLOCK_SIZE). This may take awhile!"
+	@go test -mod=readonly -benchmem -run=^$$ $(SIMAPP) -bench ^BenchmarkFullAppSimulation$$ \
 		-Enabled=true -NumBlocks=$(SIM_NUM_BLOCKS) -BlockSize=$(SIM_BLOCK_SIZE) -Commit=$(SIM_COMMIT) -timeout 24h -cpuprofile cpu.out -memprofile mem.out
 
-
-.PHONY: runsim sim-terra-nondeterminism sim-terra-custom-genesis-fast sim-terra-fast sim-terra-import-export \
-	sim-terra-simulation-after-import sim-terra-custom-genesis-multi-seed sim-terra-multi-seed \
-	sim-benchmark-invariants sim-terra-benchmark sim-terra-profile
+.PHONY: runsim test-sim-nondeterminism test-sim-custom-genesis-fast sim-import-export \
+	test-sim-simulation-after-import test-sim-custom-genesis-multi-seed test-sim-multi-seed \
+	test-sim-benchmark-invariants test-sim-fast test-sim-benchmark test-sim-profile
