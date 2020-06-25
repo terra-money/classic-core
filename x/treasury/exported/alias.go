cd9795343b8f8fe2f554ed2f7dcb6391ba8d9d04
yys <sw.yunsuk@gmail.com>
2020-06-25 14:18:21 +0900

    market client tax calculation update

---
+++ x/treasury/exported/alias.go
@@ -0,0 +1,14 @@
+// nolint:deadcode unused DONTCOVER
+package exported
+
+import (
+	"github.com/terra-project/core/x/treasury/internal/types"
+)
+
+var (
+	NewQueryTaxCapParams = types.NewQueryTaxCapParams
+
+	QuerierRoute = types.QuerierRoute
+	QueryTaxRate = types.QueryTaxRate
+	QueryTaxCap  = types.QueryTaxCap
+)
