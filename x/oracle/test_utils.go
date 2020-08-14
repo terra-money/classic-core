da22bed33fd6b584fc5e460dd937ae69411554b5
dongsam <dongsamb@gmail.com>
2020-08-14 19:11:16 +0900

    [Oracle] Exchange Rate and Ballot Rewards logic using reference terra (#381)
    * Fix Exchange Rate, Ballot Rewards logic to using reference terra
    
    * Add testcase and code refactoring

--- x/oracle/test_utils.go
+++ x/oracle/test_utils.go
@@ -61,3 +61,30 @@ func setup(t *testing.T) (keeper.TestInput, sdk.Handler) {
 
 	return input, h
 }
+
+func setupVal5(t *testing.T) (keeper.TestInput, sdk.Handler) {
+	input := keeper.CreateTestInput(t)
+	params := input.OracleKeeper.GetParams(input.Ctx)
+	params.VotePeriod = 1
+	params.SlashWindow = 100
+	params.RewardDistributionWindow = 100
+	input.OracleKeeper.SetParams(input.Ctx, params)
+	h := NewHandler(input.OracleKeeper)
+
+	sh := staking.NewHandler(input.StakingKeeper)
+
+	// Validator created
+	_, err := sh(input.Ctx, keeper.NewTestMsgCreateValidator(keeper.ValAddrs[0], keeper.PubKeys[0], stakingAmt))
+	require.NoError(t, err)
+	_, err = sh(input.Ctx, keeper.NewTestMsgCreateValidator(keeper.ValAddrs[1], keeper.PubKeys[1], stakingAmt))
+	require.NoError(t, err)
+	_, err = sh(input.Ctx, keeper.NewTestMsgCreateValidator(keeper.ValAddrs[2], keeper.PubKeys[2], stakingAmt))
+	require.NoError(t, err)
+	_, err = sh(input.Ctx, keeper.NewTestMsgCreateValidator(keeper.ValAddrs[3], keeper.PubKeys[3], stakingAmt))
+	require.NoError(t, err)
+	_, err = sh(input.Ctx, keeper.NewTestMsgCreateValidator(keeper.ValAddrs[4], keeper.PubKeys[4], stakingAmt))
+	require.NoError(t, err)
+	staking.EndBlocker(input.Ctx, input.StakingKeeper)
+
+	return input, h
+}
