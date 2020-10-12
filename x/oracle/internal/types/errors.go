570f8d493f93ec200443c3d874e68d47d0a7be71
Yun Yeo <sw.yunsuk@gmail.com>
2020-10-12 15:54:16 +0900

    convert all module error code 1 to others

--- x/oracle/internal/types/errors.go
+++ x/oracle/internal/types/errors.go
@@ -9,7 +9,6 @@ import (
 
 // Oracle Errors
 var (
-	ErrUnknowDenom           = sdkerrors.Register(ModuleName, 1, "unknown denom")
 	ErrInvalidExchangeRate   = sdkerrors.Register(ModuleName, 2, "invalid exchange rate")
 	ErrNoPrevote             = sdkerrors.Register(ModuleName, 3, "no prevote")
 	ErrNoVote                = sdkerrors.Register(ModuleName, 4, "no vote")
@@ -22,4 +21,5 @@ var (
 	ErrNoAggregatePrevote    = sdkerrors.Register(ModuleName, 11, "no aggregate prevote")
 	ErrNoAggregateVote       = sdkerrors.Register(ModuleName, 12, "no aggregate vote")
 	ErrNoTobinTax            = sdkerrors.Register(ModuleName, 13, "no tobin tax")
+	ErrUnknowDenom           = sdkerrors.Register(ModuleName, 14, "unknown denom")
 )
