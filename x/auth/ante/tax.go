56cccd07e7f7f59a277eb81d7455c0ad49806fe1
yys <sw.yunsuk@gmail.com>
2021-05-24 19:36:10 +0900

    Revert "[Feature] mempool logic update to give oracle message priority (#479)"
    This reverts commit 51de1d744e7b721b1b19c030fba82e5532be86d7.

--- x/auth/ante/tax.go
+++ x/auth/ante/tax.go
@@ -14,9 +14,6 @@ import (
 	wasmexported "github.com/terra-project/core/x/wasm/exported"
 )
 
-// MaxOracleMsgGasUsage is constant expected oracle msg gas cost
-const MaxOracleMsgGasUsage = uint64(100_000)
-
 // FeeTx defines the interface to be implemented by Tx to use the FeeDecorators
 type FeeTx interface {
 	sdk.Tx
@@ -52,14 +49,13 @@ func (tfd TaxFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool,
 
 	feeCoins := feeTx.GetFee()
 	gas := feeTx.GetGas()
-	msgs := feeTx.GetMsgs()
 
 	if !simulate {
 		// Compute taxes
 		taxes := FilterMsgAndComputeTax(ctx, tfd.treasuryKeeper, feeTx.GetMsgs())
 
 		// Mempool fee validation
-		if ctx.IsCheckTx() && !(isOracleTx(ctx, msgs) && gas <= uint64(len(msgs))*MaxOracleMsgGasUsage) {
+		if ctx.IsCheckTx() && !(isOracleTx(ctx, feeTx.GetMsgs()) && gas <= 1000000) {
 			if err := EnsureSufficientMempoolFees(ctx, gas, feeCoins, taxes); err != nil {
 				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInsufficientFee, err.Error())
 			}
