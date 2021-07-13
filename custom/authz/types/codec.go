471cd878b11b21f10511e272621b850a690eb6f3
yys <yun@terra.money>
2021-07-13 14:34:25 +0900

    add amino support for StakeAuthorization (#512)

--- custom/authz/types/codec.go
+++ custom/authz/types/codec.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/authz"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // RegisterLegacyAminoCodec registers the necessary x/authz interfaces and concrete types
@@ -14,6 +15,7 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&authz.MsgGrant{}, "msgauth/MsgGrantAuthorization", nil)
 	cdc.RegisterConcrete(&authz.MsgRevoke{}, "msgauth/MsgRevokeAuthorization", nil)
 	cdc.RegisterConcrete(&authz.MsgExec{}, "msgauth/MsgExecAuthorized", nil)
-	cdc.RegisterConcrete(&banktypes.SendAuthorization{}, "msgauth/SendAuthorization", nil)
 	cdc.RegisterConcrete(&authz.GenericAuthorization{}, "msgauth/GenericAuthorization", nil)
+	cdc.RegisterConcrete(&banktypes.SendAuthorization{}, "msgauth/SendAuthorization", nil)
+	cdc.RegisterConcrete(&stakingtypes.StakeAuthorization{}, "msgauth/StakeAuthorization", nil)
 }
