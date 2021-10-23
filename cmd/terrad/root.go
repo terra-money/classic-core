2fa887f19ede827c63c8ad4eed6bc1af0a583280
Jacob Gadikian <jacobgadikian@gmail.com>
2021-10-23 21:30:04 +0700

    Update root.go
    change how flags are handled

--- cmd/terrad/root.go
+++ cmd/terrad/root.go
@@ -1,256 +1,256 @@
 package main
 
 import (
-	"errors"
-	"io"
-	"os"
-	"path/filepath"
+        "errors"
+        "io"
+        "os"
+        "path/filepath"
 
-	"github.com/spf13/cast"
-	"github.com/spf13/cobra"
-	tmcli "github.com/tendermint/tendermint/libs/cli"
-	"github.com/tendermint/tendermint/libs/log"
-	dbm "github.com/tendermint/tm-db"
+        "github.com/spf13/cast"
+        "github.com/spf13/cobra"
+        tmcli "github.com/tendermint/tendermint/libs/cli"
+        "github.com/tendermint/tendermint/libs/log"
+        dbm "github.com/tendermint/tm-db"
 
-	"github.com/cosmos/cosmos-sdk/baseapp"
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/config"
-	"github.com/cosmos/cosmos-sdk/client/debug"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/client/keys"
-	"github.com/cosmos/cosmos-sdk/client/rpc"
-	"github.com/cosmos/cosmos-sdk/server"
-	servertypes "github.com/cosmos/cosmos-sdk/server/types"
-	"github.com/cosmos/cosmos-sdk/snapshots"
-	"github.com/cosmos/cosmos-sdk/store"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	"github.com/cosmos/cosmos-sdk/x/crisis"
-	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
+        "github.com/cosmos/cosmos-sdk/baseapp"
+        "github.com/cosmos/cosmos-sdk/client"
+        "github.com/cosmos/cosmos-sdk/client/config"
+        "github.com/cosmos/cosmos-sdk/client/debug"
+        "github.com/cosmos/cosmos-sdk/client/flags"
+        "github.com/cosmos/cosmos-sdk/client/keys"
+        "github.com/cosmos/cosmos-sdk/client/rpc"
+        "github.com/cosmos/cosmos-sdk/server"
+        servertypes "github.com/cosmos/cosmos-sdk/server/types"
+        "github.com/cosmos/cosmos-sdk/snapshots"
+        "github.com/cosmos/cosmos-sdk/store"
+        sdk "github.com/cosmos/cosmos-sdk/types"
+        authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
+        "github.com/cosmos/cosmos-sdk/x/auth/types"
+        banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+        "github.com/cosmos/cosmos-sdk/x/crisis"
+        genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 
-	terraapp "github.com/terra-money/core/app"
-	terralegacy "github.com/terra-money/core/app/legacy"
-	"github.com/terra-money/core/app/params"
-	authcustomcli "github.com/terra-money/core/custom/auth/client/cli"
-	core "github.com/terra-money/core/types"
-	wasmconfig "github.com/terra-money/core/x/wasm/config"
+        terraapp "github.com/terra-money/core/app"
+        terralegacy "github.com/terra-money/core/app/legacy"
+        "github.com/terra-money/core/app/params"
+        authcustomcli "github.com/terra-money/core/custom/auth/client/cli"
+        core "github.com/terra-money/core/types"
+        wasmconfig "github.com/terra-money/core/x/wasm/config"
 )
 
 // NewRootCmd creates a new root command for terrad. It is called once in the
 // main function.
 func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
-	encodingConfig := terraapp.MakeEncodingConfig()
+        encodingConfig := terraapp.MakeEncodingConfig()
 
-	sdkConfig := sdk.GetConfig()
-	sdkConfig.SetCoinType(core.CoinType)
-	sdkConfig.SetFullFundraiserPath(core.FullFundraiserPath)
-	sdkConfig.SetBech32PrefixForAccount(core.Bech32PrefixAccAddr, core.Bech32PrefixAccPub)
-	sdkConfig.SetBech32PrefixForValidator(core.Bech32PrefixValAddr, core.Bech32PrefixValPub)
-	sdkConfig.SetBech32PrefixForConsensusNode(core.Bech32PrefixConsAddr, core.Bech32PrefixConsPub)
-	sdkConfig.SetAddressVerifier(core.AddressVerifier)
-	sdkConfig.Seal()
+        sdkConfig := sdk.GetConfig()
+        sdkConfig.SetCoinType(core.CoinType)
+        sdkConfig.SetFullFundraiserPath(core.FullFundraiserPath)
+        sdkConfig.SetBech32PrefixForAccount(core.Bech32PrefixAccAddr, core.Bech32PrefixAccPub)
+        sdkConfig.SetBech32PrefixForValidator(core.Bech32PrefixValAddr, core.Bech32PrefixValPub)
+        sdkConfig.SetBech32PrefixForConsensusNode(core.Bech32PrefixConsAddr, core.Bech32PrefixConsPub)
+        sdkConfig.SetAddressVerifier(core.AddressVerifier)
+        sdkConfig.Seal()
 
-	initClientCtx := client.Context{}.
-		WithCodec(encodingConfig.Marshaler).
-		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
-		WithTxConfig(encodingConfig.TxConfig).
-		WithLegacyAmino(encodingConfig.Amino).
-		WithInput(os.Stdin).
-		WithAccountRetriever(types.AccountRetriever{}).
-		WithHomeDir(terraapp.DefaultNodeHome).
-		WithViper("TERRA")
+        initClientCtx := client.Context{}.
+                WithCodec(encodingConfig.Marshaler).
+                WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
+                WithTxConfig(encodingConfig.TxConfig).
+                WithLegacyAmino(encodingConfig.Amino).
+                WithInput(os.Stdin).
+                WithAccountRetriever(types.AccountRetriever{}).
+                WithHomeDir(terraapp.DefaultNodeHome).
+                WithViper("TERRA")
 
-	rootCmd := &cobra.Command{
-		Use:   "terrad",
-		Short: "Stargate Terra App",
-		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
-			// set the default command outputs
-			cmd.SetOut(cmd.OutOrStdout())
-			cmd.SetErr(cmd.ErrOrStderr())
+        rootCmd := &cobra.Command{
+                Use:   "terrad",
+                Short: "Stargate Terra App",
+                PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+                        // set the default command outputs
+                        cmd.SetOut(cmd.OutOrStdout())
+                        cmd.SetErr(cmd.ErrOrStderr())
 
-			initClientCtx = client.ReadHomeFlag(initClientCtx, cmd)
-			initClientCtx, err := config.ReadFromClientConfig(initClientCtx)
-			if err != nil {
-				return err
-			}
+                        initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
+                        initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
+                        if err != nil {
+                                return err
+                        }
 
-			if err := client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
-				return err
-			}
+                        if err := client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
+                                return err
+                        }
 
-			terraAppTemplate, terraAppConfig := initAppConfig()
+                        terraAppTemplate, terraAppConfig := initAppConfig()
 
-			return server.InterceptConfigsPreRunHandler(cmd, terraAppTemplate, terraAppConfig)
-		},
-	}
+                        return server.InterceptConfigsPreRunHandler(cmd, terraAppTemplate, terraAppConfig)
+                },
+        }
 
-	initRootCmd(rootCmd, encodingConfig)
+        initRootCmd(rootCmd, encodingConfig)
 
-	return rootCmd, encodingConfig
+        return rootCmd, encodingConfig
 }
 
 func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
-	// TODO check gaia before make release candidate
-	// authclient.Codec = encodingConfig.Marshaler
+        // TODO check gaia before make release candidate
+        // authclient.Codec = encodingConfig.Marshaler
 
-	rootCmd.AddCommand(
-		genutilcli.InitCmd(terraapp.ModuleBasics, terraapp.DefaultNodeHome),
-		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, terraapp.DefaultNodeHome),
-		terralegacy.MigrateGenesisCmd(),
-		genutilcli.GenTxCmd(terraapp.ModuleBasics, encodingConfig.TxConfig, banktypes.GenesisBalancesIterator{}, terraapp.DefaultNodeHome),
-		genutilcli.ValidateGenesisCmd(terraapp.ModuleBasics),
-		AddGenesisAccountCmd(terraapp.DefaultNodeHome),
-		tmcli.NewCompletionCmd(rootCmd, true),
-		testnetCmd(terraapp.ModuleBasics, banktypes.GenesisBalancesIterator{}),
-		debug.Cmd(),
-	)
+        rootCmd.AddCommand(
+                genutilcli.InitCmd(terraapp.ModuleBasics, terraapp.DefaultNodeHome),
+                genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, terraapp.DefaultNodeHome),
+                terralegacy.MigrateGenesisCmd(),
+                genutilcli.GenTxCmd(terraapp.ModuleBasics, encodingConfig.TxConfig, banktypes.GenesisBalancesIterator{}, terraapp.DefaultNodeHome),
+                genutilcli.ValidateGenesisCmd(terraapp.ModuleBasics),
+                AddGenesisAccountCmd(terraapp.DefaultNodeHome),
+                tmcli.NewCompletionCmd(rootCmd, true),
+                testnetCmd(terraapp.ModuleBasics, banktypes.GenesisBalancesIterator{}),
+                debug.Cmd(),
+        )
 
-	a := appCreator{encodingConfig}
-	server.AddCommands(rootCmd, terraapp.DefaultNodeHome, a.newApp, a.appExport, addModuleInitFlags)
+        a := appCreator{encodingConfig}
+        server.AddCommands(rootCmd, terraapp.DefaultNodeHome, a.newApp, a.appExport, addModuleInitFlags)
 
-	// add keybase, auxiliary RPC, query, and tx child commands
-	rootCmd.AddCommand(
-		rpc.StatusCommand(),
-		queryCommand(),
-		txCommand(),
-		keys.Commands(terraapp.DefaultNodeHome),
-	)
+        // add keybase, auxiliary RPC, query, and tx child commands
+        rootCmd.AddCommand(
+                rpc.StatusCommand(),
+                queryCommand(),
+                txCommand(),
+                keys.Commands(terraapp.DefaultNodeHome),
+        )
 
-	// add rosetta commands
-	rootCmd.AddCommand(server.RosettaCommand(encodingConfig.InterfaceRegistry, encodingConfig.Marshaler))
+        // add rosetta commands
+        rootCmd.AddCommand(server.RosettaCommand(encodingConfig.InterfaceRegistry, encodingConfig.Marshaler))
 }
 
 func addModuleInitFlags(startCmd *cobra.Command) {
-	crisis.AddModuleInitFlags(startCmd)
+        crisis.AddModuleInitFlags(startCmd)
 }
 
 func queryCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                        "query",
-		Aliases:                    []string{"q"},
-		Short:                      "Querying subcommands",
-		DisableFlagParsing:         true,
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
-	}
+        cmd := &cobra.Command{
+                Use:                        "query",
+                Aliases:                    []string{"q"},
+                Short:                      "Querying subcommands",
+                DisableFlagParsing:         true,
+                SuggestionsMinimumDistance: 2,
+                RunE:                       client.ValidateCmd,
+        }
 
-	cmd.AddCommand(
-		authcmd.GetAccountCmd(),
-		rpc.ValidatorCommand(),
-		rpc.BlockCommand(),
-		authcmd.QueryTxsByEventsCmd(),
-		authcmd.QueryTxCmd(),
-		authcustomcli.GetTxFeesEstimateCommand(),
-	)
+        cmd.AddCommand(
+                authcmd.GetAccountCmd(),
+                rpc.ValidatorCommand(),
+                rpc.BlockCommand(),
+                authcmd.QueryTxsByEventsCmd(),
+                authcmd.QueryTxCmd(),
+                authcustomcli.GetTxFeesEstimateCommand(),
+        )
 
-	terraapp.ModuleBasics.AddQueryCommands(cmd)
-	cmd.PersistentFlags().String(flags.FlagChainID, "", "The network chain ID")
+        terraapp.ModuleBasics.AddQueryCommands(cmd)
+        cmd.PersistentFlags().String(flags.FlagChainID, "", "The network chain ID")
 
-	return cmd
+        return cmd
 }
 
 func txCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                        "tx",
-		Short:                      "Transactions subcommands",
-		DisableFlagParsing:         true,
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
-	}
+        cmd := &cobra.Command{
+                Use:                        "tx",
+                Short:                      "Transactions subcommands",
+                DisableFlagParsing:         true,
+                SuggestionsMinimumDistance: 2,
+                RunE:                       client.ValidateCmd,
+        }
 
-	cmd.AddCommand(
-		authcmd.GetSignCommand(),
-		authcmd.GetSignBatchCommand(),
-		authcmd.GetMultiSignCommand(),
-		authcmd.GetMultiSignBatchCmd(),
-		authcmd.GetValidateSignaturesCommand(),
-		flags.LineBreak,
-		authcmd.GetBroadcastCommand(),
-		authcmd.GetEncodeCommand(),
-		authcmd.GetDecodeCommand(),
-		flags.LineBreak,
-	)
+        cmd.AddCommand(
+                authcmd.GetSignCommand(),
+                authcmd.GetSignBatchCommand(),
+                authcmd.GetMultiSignCommand(),
+                authcmd.GetMultiSignBatchCmd(),
+                authcmd.GetValidateSignaturesCommand(),
+                flags.LineBreak,
+                authcmd.GetBroadcastCommand(),
+                authcmd.GetEncodeCommand(),
+                authcmd.GetDecodeCommand(),
+                flags.LineBreak,
+        )
 
-	terraapp.ModuleBasics.AddTxCommands(cmd)
-	cmd.PersistentFlags().String(flags.FlagChainID, "", "The network chain ID")
+        terraapp.ModuleBasics.AddTxCommands(cmd)
+        cmd.PersistentFlags().String(flags.FlagChainID, "", "The network chain ID")
 
-	return cmd
+        return cmd
 }
 
 type appCreator struct {
-	encodingConfig params.EncodingConfig
+        encodingConfig params.EncodingConfig
 }
 
 // newApp is an AppCreator
 func (a appCreator) newApp(logger log.Logger, db dbm.DB, traceStore io.Writer, appOpts servertypes.AppOptions) servertypes.Application {
-	var cache sdk.MultiStorePersistentCache
+        var cache sdk.MultiStorePersistentCache
 
-	if cast.ToBool(appOpts.Get(server.FlagInterBlockCache)) {
-		cache = store.NewCommitKVStoreCacheManager()
-	}
+        if cast.ToBool(appOpts.Get(server.FlagInterBlockCache)) {
+                cache = store.NewCommitKVStoreCacheManager()
+        }
 
-	skipUpgradeHeights := make(map[int64]bool)
-	for _, h := range cast.ToIntSlice(appOpts.Get(server.FlagUnsafeSkipUpgrades)) {
-		skipUpgradeHeights[int64(h)] = true
-	}
+        skipUpgradeHeights := make(map[int64]bool)
+        for _, h := range cast.ToIntSlice(appOpts.Get(server.FlagUnsafeSkipUpgrades)) {
+                skipUpgradeHeights[int64(h)] = true
+        }
 
-	pruningOpts, err := server.GetPruningOptionsFromFlags(appOpts)
-	if err != nil {
-		panic(err)
-	}
+        pruningOpts, err := server.GetPruningOptionsFromFlags(appOpts)
+        if err != nil {
+                panic(err)
+        }
 
-	snapshotDir := filepath.Join(cast.ToString(appOpts.Get(flags.FlagHome)), "data", "snapshots")
-	snapshotDB, err := sdk.NewLevelDB("metadata", snapshotDir)
-	if err != nil {
-		panic(err)
-	}
-	snapshotStore, err := snapshots.NewStore(snapshotDB, snapshotDir)
-	if err != nil {
-		panic(err)
-	}
+        snapshotDir := filepath.Join(cast.ToString(appOpts.Get(flags.FlagHome)), "data", "snapshots")
+        snapshotDB, err := sdk.NewLevelDB("metadata", snapshotDir)
+        if err != nil {
+                panic(err)
+        }
+        snapshotStore, err := snapshots.NewStore(snapshotDB, snapshotDir)
+        if err != nil {
+                panic(err)
+        }
 
-	return terraapp.NewTerraApp(
-		logger, db, traceStore, true, skipUpgradeHeights,
-		cast.ToString(appOpts.Get(flags.FlagHome)),
-		cast.ToUint(appOpts.Get(server.FlagInvCheckPeriod)),
-		a.encodingConfig,
-		appOpts,
-		wasmconfig.GetConfig(appOpts),
-		baseapp.SetPruning(pruningOpts),
-		baseapp.SetMinGasPrices(cast.ToString(appOpts.Get(server.FlagMinGasPrices))),
-		baseapp.SetHaltHeight(cast.ToUint64(appOpts.Get(server.FlagHaltHeight))),
-		baseapp.SetHaltTime(cast.ToUint64(appOpts.Get(server.FlagHaltTime))),
-		baseapp.SetMinRetainBlocks(cast.ToUint64(appOpts.Get(server.FlagMinRetainBlocks))),
-		baseapp.SetInterBlockCache(cache),
-		baseapp.SetTrace(cast.ToBool(appOpts.Get(server.FlagTrace))),
-		baseapp.SetIndexEvents(cast.ToStringSlice(appOpts.Get(server.FlagIndexEvents))),
-		baseapp.SetSnapshotStore(snapshotStore),
-		baseapp.SetSnapshotInterval(cast.ToUint64(appOpts.Get(server.FlagStateSyncSnapshotInterval))),
-		baseapp.SetSnapshotKeepRecent(cast.ToUint32(appOpts.Get(server.FlagStateSyncSnapshotKeepRecent))),
-	)
+        return terraapp.NewTerraApp(
+                logger, db, traceStore, true, skipUpgradeHeights,
+                cast.ToString(appOpts.Get(flags.FlagHome)),
+                cast.ToUint(appOpts.Get(server.FlagInvCheckPeriod)),
+                a.encodingConfig,
+                appOpts,
+                wasmconfig.GetConfig(appOpts),
+                baseapp.SetPruning(pruningOpts),
+                baseapp.SetMinGasPrices(cast.ToString(appOpts.Get(server.FlagMinGasPrices))),
+                baseapp.SetHaltHeight(cast.ToUint64(appOpts.Get(server.FlagHaltHeight))),
+                baseapp.SetHaltTime(cast.ToUint64(appOpts.Get(server.FlagHaltTime))),
+                baseapp.SetMinRetainBlocks(cast.ToUint64(appOpts.Get(server.FlagMinRetainBlocks))),
+                baseapp.SetInterBlockCache(cache),
+                baseapp.SetTrace(cast.ToBool(appOpts.Get(server.FlagTrace))),
+                baseapp.SetIndexEvents(cast.ToStringSlice(appOpts.Get(server.FlagIndexEvents))),
+                baseapp.SetSnapshotStore(snapshotStore),
+                baseapp.SetSnapshotInterval(cast.ToUint64(appOpts.Get(server.FlagStateSyncSnapshotInterval))),
+                baseapp.SetSnapshotKeepRecent(cast.ToUint32(appOpts.Get(server.FlagStateSyncSnapshotKeepRecent))),
+        )
 }
 
 func (a appCreator) appExport(
-	logger log.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailAllowedAddrs []string,
-	appOpts servertypes.AppOptions) (servertypes.ExportedApp, error) {
+        logger log.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailAllowedAddrs []string,
+        appOpts servertypes.AppOptions) (servertypes.ExportedApp, error) {
 
-	homePath, ok := appOpts.Get(flags.FlagHome).(string)
-	if !ok || homePath == "" {
-		return servertypes.ExportedApp{}, errors.New("application home not set")
-	}
+        homePath, ok := appOpts.Get(flags.FlagHome).(string)
+        if !ok || homePath == "" {
+                return servertypes.ExportedApp{}, errors.New("application home not set")
+        }
 
-	var terraApp *terraapp.TerraApp
-	if height != -1 {
-		terraApp = terraapp.NewTerraApp(logger, db, traceStore, false, map[int64]bool{}, homePath, cast.ToUint(appOpts.Get(server.FlagInvCheckPeriod)), a.encodingConfig, appOpts, wasmconfig.DefaultConfig())
+        var terraApp *terraapp.TerraApp
+        if height != -1 {
+                terraApp = terraapp.NewTerraApp(logger, db, traceStore, false, map[int64]bool{}, homePath, cast.ToUint(appOpts.Get(server.FlagInvCheckPeriod)), a.encodingConfig, appOpts, wasmconfig.DefaultConfig())
 
-		if err := terraApp.LoadHeight(height); err != nil {
-			return servertypes.ExportedApp{}, err
-		}
-	} else {
-		terraApp = terraapp.NewTerraApp(logger, db, traceStore, true, map[int64]bool{}, homePath, cast.ToUint(appOpts.Get(server.FlagInvCheckPeriod)), a.encodingConfig, appOpts, wasmconfig.DefaultConfig())
-	}
+                if err := terraApp.LoadHeight(height); err != nil {
+                        return servertypes.ExportedApp{}, err
+                }
+        } else {
+                terraApp = terraapp.NewTerraApp(logger, db, traceStore, true, map[int64]bool{}, homePath, cast.ToUint(appOpts.Get(server.FlagInvCheckPeriod)), a.encodingConfig, appOpts, wasmconfig.DefaultConfig())
+        }
 
-	return terraApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs)
+        return terraApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs)
 }
