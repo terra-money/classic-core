fb1b646c8c88698e472aff6846978544411b3697
Yun <sw.yunsuk@gmail.com>
2022-04-12 14:22:30 +0900

    add inter chain account module and ibc router module & bump ibc-go to v3

---
+++ app/const.go
@@ -0,0 +1,33 @@
+package app
+
+const (
+	appName     = "TerraApp"
+	upgradeName = "columbus-6"
+
+	authzMsgExec                        = "/cosmos.authz.v1beta1.MsgExec"
+	authzMsgGrant                       = "/cosmos.authz.v1beta1.MsgGrant"
+	authzMsgRevoke                      = "/cosmos.authz.v1beta1.MsgRevoke"
+	bankMsgSend                         = "/cosmos.bank.v1beta1.MsgSend"
+	bankMsgMultiSend                    = "/cosmos.bank.v1beta1.MsgMultiSend"
+	distrMsgSetWithdrawAddr             = "/cosmos.distribution.v1beta1.MsgSetWithdrawAddress"
+	distrMsgWithdrawValidatorCommission = "/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission"
+	distrMsgFundCommunityPool           = "/cosmos.distribution.v1beta1.MsgFundCommunityPool"
+	distrMsgWithdrawDelegatorReward     = "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward"
+	feegrantMsgGrantAllowance           = "/cosmos.feegrant.v1beta1.MsgGrantAllowance"
+	feegrantMsgRevokeAllowance          = "/cosmos.feegrant.v1beta1.MsgRevokeAllowance"
+	govMsgVoteWeighted                  = "/cosmos.gov.v1beta1.MsgVoteWeighted"
+	govMsgSubmitProposal                = "/cosmos.gov.v1beta1.MsgSubmitProposal"
+	govMsgDeposit                       = "/cosmos.gov.v1beta1.MsgDeposit"
+	govMsgVote                          = "/cosmos.gov.v1beta1.MsgVote"
+	stakingMsgEditValidator             = "/cosmos.staking.v1beta1.MsgEditValidator"
+	stakingMsgDelegate                  = "/cosmos.staking.v1beta1.MsgDelegate"
+	stakingMsgUndelegate                = "/cosmos.staking.v1beta1.MsgUndelegate"
+	stakingMsgBeginRedelegate           = "/cosmos.staking.v1beta1.MsgBeginRedelegate"
+	stakingMsgCreateValidator           = "/cosmos.staking.v1beta1.MsgCreateValidator"
+	vestingMsgCreateVestingAccount      = "/cosmos.vesting.v1beta1.MsgCreateVestingAccount"
+	transferMsgTransfer                 = "/ibc.applications.transfer.v1.MsgTransfer"
+	liquidityMsgCreatePool              = "/tendermint.liquidity.v1beta1.MsgCreatePool"
+	liquidityMsgSwapWithinBatch         = "/tendermint.liquidity.v1beta1.MsgSwapWithinBatch"
+	liquidityMsgDepositWithinBatch      = "/tendermint.liquidity.v1beta1.MsgDepositWithinBatch"
+	liquidityMsgWithdrawWithinBatch     = "/tendermint.liquidity.v1beta1.MsgWithdrawWithinBatch"
+)
