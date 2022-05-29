bc15ce931ee3e5094ae11f860006d61fb8bfc534
ZaradarDFDS <toban@dfds.com>
2022-05-30 01:06:55 +0200

    Added TODOs to re-enable market keeper swaps once algo is fixed

--- types/fork/height.go
+++ types/fork/height.go
@@ -2,3 +2,6 @@ package fork
 
 // SwapDisableForkHeight - make min spread to 100% to disable swap
 const SwapDisableForkHeight = 7607790
+
+// TODO: Determine block height for re-enabling swaps once market keeper is fixed
+const SwapFixedForkHeight = -1
