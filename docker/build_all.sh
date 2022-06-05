f55ff35a7334d20b10fdb9bdf2ae67d13286271a
ZaradarDFDS <toban@dfds.com>
2022-06-05 21:29:16 +0200

    Removed whitespace in build_all.sh

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -11,4 +11,4 @@ git checkout -
 popd
 
 docker build --build-arg version=$VERSION --build-arg chainid=columbus-5 -t terramoney/core-node:$VERSION .
-docker build --build-arg version=$VERSION --build-arg chainid=bombay-12 -t terramoney/core-node:$VERSION-testnet . 
+docker build --build-arg version=$VERSION --build-arg chainid=bombay-12 -t terramoney/core-node:$VERSION-testnet .
