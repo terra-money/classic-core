8a53848f709116dd92cead3caf4ed8f590d26d28
ZaradarDFDS <toban@dfds.com>
2022-06-05 20:17:58 +0200

    - Updated comment

--- types/fork/height.go
+++ types/fork/height.go
@@ -3,5 +3,5 @@ package fork
 // SwapDisableForkHeight - make min spread to 100% to disable swap
 const SwapDisableForkHeight = 7607790
 
-// SwapEnableForkHeight - make min spread to 50% to enable swap with new guards
+// SwapEnableForkHeight - make min spread to 98% to enable swap with new guards
 const SwapEnableForkHeight = 9000000
